36f61f13eeafa5ccef2c7dbac61308b069e5b1e2
Tim Scott <tscott@lunaversesoftware.com>
2016-12-22 10:31:44 -0600

    Trying a module.

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ import {AppRegistry} from 'react-native';
 import ApolloClient, {createNetworkInterface} from 'apollo-client';
 import {ApolloProvider} from 'react-apollo';
 import Scene from './components/Scene';
+import Config from 'react-native-config';
 
 const networkInterface = createNetworkInterface({
-  uri: 'http://192.168.1.175:8080/graphql'
+  uri: Config.API_URL
 });
 const client = new ApolloClient({
   networkInterface: networkInterface
